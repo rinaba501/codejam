0fd246206caafab11da8a4f8efbe10e23d0c12f0
Unknown <rinaba@umich.edu>
2018-11-26 13:16:55 -0500

    VER: saving is correct

--- 2018/qualification/saving-the-universe-again/saving.cpp
+++ 2018/qualification/saving-the-universe-again/saving.cpp
@@ -10,7 +10,38 @@ int main() {
 
     for (size_t i = 0; i < num_tests; ++i) {
         cin >> max_total_damage >> P;
-        cout << P << endl;
+        vector<int> vals;
+        int damage = 1;
+        int total_damage = 0;
+        for (char c : P) {
+            if (c == 'C') {
+                damage *= 2;
+            } else {
+                vals.push_back(damage);
+                total_damage += damage;
+            }
+        }
+        int length = vals.size();
+        if (length > max_total_damage) {
+            cout << "Case #" << i + 1 << ": IMPOSSIBLE\n";
+        } else {
+            int hack_count = 0;
+            int idx = length;
+            while (total_damage > max_total_damage) {
+                if (idx == length) {
+                    int curr_max_damage = vals[length - 1];
+                    idx = length - 1;
+                    while(idx && vals[idx - 1] == curr_max_damage) {
+                        idx--;
+                    }
+                }
+                vals[idx] /= 2;
+                total_damage -= vals[idx];
+                ++idx;
+                hack_count++;
+            }
+            cout << "Case #" << i + 1 << ": " << hack_count << "\n";
+        }
     }
 
     return 0;
